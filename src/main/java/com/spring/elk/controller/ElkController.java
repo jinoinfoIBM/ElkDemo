0bc34ab86dd37dca0aebfbde3f5ff8ed3b8ddd70
jinoinfoIBM <jinoinfo@gmail.com>
2023-02-20 16:34:06 +0530

    New ElkDemo project created

---
+++ src/main/java/com/spring/elk/controller/ElkController.java
@@ -0,0 +1,67 @@
+package com.spring.elk.controller;
+
+
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class ElkController {
+	
+	@SuppressWarnings("unused")
+	private static final Logger Log= LogManager.getLogger(ElkController.class);
+	
+	//@Autowired
+	RestTemplate restTemplate;
+	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+	@RequestMapping( value="/elkmsg")
+	public String getMessage() {
+		
+		String response = "Welcome to Elk Testing with Log4J2 "+ new Date();
+		
+		Log.log(Level.INFO, response);
+		
+		return response;
+		
+		
+		}
+	
+	@RequestMapping(value= "/exception")
+	public String exception() {
+		
+		String response ="";
+		
+		
+		try {
+			throw new Exception("Exception for ELK testing");
+			
+		}catch(Exception ex) {
+			ex.printStackTrace();
+			Log.error(ex);
+			
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			ex.printStackTrace(pw);
+			String stackTrace =sw.toString();
+			Log.error("Exception - " + stackTrace);
+			response = stackTrace;
+		}
+		return response;
+	}
+	
+}
